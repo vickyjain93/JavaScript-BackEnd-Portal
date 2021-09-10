f5419189f9288438759915cb2715bbb5389a3edd
vickyjain93 <vicksjain2@gmail.com>
2021-09-10 12:45:01 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,23 @@
+﻿require('rootpath')();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const jwt = require('_helpers/jwt');
+const errorHandler = require('_helpers/error-handler');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use(jwt());
+
+app.use('/users', require('./controllers/users.controller'));
+app.use('/booking', require('./controllers/booking.controller'));
+app.use('/partner', require('./controllers/partner.controller'));
+app.use('/agent', require('./controllers/agent.controller'));
+app.use('/villas', require('./controllers/villas.controller'));
+
+app.use(errorHandler);
+
+const server = app.listen(process.env.PORT || 5000)
